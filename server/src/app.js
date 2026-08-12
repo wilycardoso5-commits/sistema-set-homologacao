e7399bf032403e7f7cdbb8dc5f50436ab1b5d3ab
Wily Cardoso <wilycardoso5@gmail.com>
2026-08-12 10:33:24 -0300

    Aumenta limite de payload para 50mb e ajusta chave set_banco_pdf_v1

--- server/src/app.js
+++ server/src/app.js
@@ -37,8 +37,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Limite de tamanho de JSON no body (Segurança)
-app.use(express.json({ limit: '2mb' }));
-app.use(express.urlencoded({ extended: true, limit: '2mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Sessão Segura com armazenamento no PostgreSQL
 app.use(
